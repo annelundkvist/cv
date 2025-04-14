a9a67e9144089eff1424ab4c92e581102270fa54
annelundkvist <anneyalundkvist@gmail.com>
2025-04-14 16:57:27 +0200

    change in home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -30,12 +30,11 @@ export function Home(){
             <div className='download'>
 
                 <div className='button-container'>
-                    <a href='/pdf/cv.pdf' className='button'
-                    download>
+                <a href={`${process.env.PUBLIC_URL}/pdf/cv.pdf`} download className='button'>
                     
                     <p>CV</p>
                     </a>
-                    <a href='/pdf/eksaminspapir.pdf' download className='button' >
+                    <a href={`${process.env.PUBLIC_URL}/pdf/eksaminspapir.pdf`} download className='button' >
                     <p>Eksaminspapir</p>
                     </a>
                 </div>
